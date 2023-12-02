871b24cb96574305c32ce848e661ab1b49f55ca0
Olivier Kermorgant <olivier_kermorgant@yahoo.fr>
2023-12-02 14:46:17 +0100

    cam ok / imu has wrong frame

--- launch/upload_launch.py
+++ launch/upload_launch.py
@@ -9,11 +9,7 @@ def generate_launch_description():
     sl = SimpleLauncher(use_sim_time=True)
     
     sl.declare_arg('name', 'turtlebot')
-    sl.declare_arg('x', 0.)
-    sl.declare_arg('y', 0.)
-    sl.declare_arg('z', .5)
-    sl.declare_arg('yaw', 0.)
-    
+    sl.declare_gazebo_axes(x = 0., y = 0., z = .3, yaw = 0.)
     sl.declare_arg('gui', False)
     
     name = sl.arg('name')
@@ -21,29 +17,38 @@ def generate_launch_description():
     with sl.group(ns=name):
         
         sl.robot_state_publisher('turtlebot3_xacro','turtlebot3_' + TURTLEBOT3_MODEL + '.urdf.xacro',
-                                 xacro_args={'prefix': sl.name_join(name, '/')})
+                                 xacro_args={'prefix': name+'/'})
 
-        sl.spawn_gz_model(name,
-                          spawn_args = [[f'-{tag} ', sl.arg(axis)] for axis,tag in (('x','x'),('y','y'),('z','z'),('yaw','Y'))])
+        sl.spawn_gz_model(name, spawn_args = [sl.gazebo_axes_args()])
         
-        # TODO run bridges for odometry / camera / scanner
         bridges = [GazeboBridge.clock()]
         # joint states
         gz_js_topic = sl.name_join(GazeboBridge.model_prefix(name),'/joint_state')
         bridges.append(GazeboBridge(gz_js_topic, 'joint_states', 'sensor_msgs/JointState', GazeboBridge.gz2ros))
         
-        # odom
-        bridges.append(GazeboBridge(GazeboBridge.model_topic(name, 'odometry'),
+        # odom + base TF
+        bridges.append(GazeboBridge('/model'/name/'odometry',
                                      'odom', 'nav_msgs/Odometry', GazeboBridge.gz2ros))
+        bridges.append(GazeboBridge('/model'/name/'tf',
+                                    '/tf', 'tf2_msgs/msg/TFMessage', GazeboBridge.gz2ros))
         
         # diff drive
-        bridges.append(GazeboBridge(GazeboBridge.model_topic(name, 'cmd_vel'),
+        bridges.append(GazeboBridge('/model'/name/'cmd_vel',
                                      'cmd_vel', 'geometry_msgs/Twist', GazeboBridge.ros2gz))
+
+        # imu
+        bridges.append(GazeboBridge(name/'imu',
+                                    'imu', 'sensor_msgs/Imu', GazeboBridge.gz2ros))
+
+        # cam
+        bridges.append(GazeboBridge(name/'image', 'image', 'sensor_msgs/Image', GazeboBridge.gz2ros))
+
+        # LiDAR
+        bridges.append(GazeboBridge(name/'scan', 'scan', 'sensor_msgs/LaserScan', GazeboBridge.gz2ros))
+
         sl.create_gz_bridge(bridges)
         
         with sl.group(if_arg='gui'):
             sl.node('slider_publisher', arguments=[sl.find('slider_publisher', 'Twist.yaml')])
         
-
-
     return sl.launch_description()
