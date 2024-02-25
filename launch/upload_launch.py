f3c9fba7637718bd8feb5191f013157e3beddd3d
Olivier Kermorgant <olivier_kermorgant@yahoo.fr>
2024-02-25 07:35:07 +0100

    add ground truth flag

--- launch/upload_launch.py
+++ launch/upload_launch.py
@@ -1,16 +1,17 @@
 import os
 from simple_launch import SimpleLauncher, GazeboBridge
 
+sl = SimpleLauncher(use_sim_time=True)
 
-def generate_launch_description():
+sl.declare_arg('name', 'turtlebot')
+sl.declare_gazebo_axes(x = 0., y = 0., z = .3, yaw = 0.)
+sl.declare_arg('gui', False, description = 'If we want sliders to control the velocity manually')
+sl.declare_arg('gt', False, description = 'If Gazebo should publish the odometry or the ground truth')
+
+
+def launch_setup():
 
     TURTLEBOT3_MODEL = os.environ['TURTLEBOT3_MODEL']
-    
-    sl = SimpleLauncher(use_sim_time=True)
-    
-    sl.declare_arg('name', 'turtlebot')
-    sl.declare_gazebo_axes(x = 0., y = 0., z = .3, yaw = 0.)
-    sl.declare_arg('gui', False)
     
     name = sl.arg('name')
     
@@ -21,28 +22,36 @@ def generate_launch_description():
 
         sl.spawn_gz_model(name, spawn_args = [sl.gazebo_axes_args()])
         
-        bridges = [GazeboBridge.clock()]
+        bridges = []
         # joint states
         gz_js_topic = sl.name_join(GazeboBridge.model_prefix(name),'/joint_state')
         bridges.append(GazeboBridge(gz_js_topic, 'joint_states', 'sensor_msgs/JointState', GazeboBridge.gz2ros))
         
-        bridges.append(GazeboBridge(GazeboBridge.model_topic(name, 'odometry'),
-                                     'odom', 'nav_msgs/Odometry', GazeboBridge.gz2ros))
-        bridges.append(GazeboBridge('/model'/name/'tf',
-                                    '/tf', 'tf2_msgs/msg/TFMessage', GazeboBridge.gz2ros))
+        if sl.arg('gt'):
+            bridges.append(GazeboBridge(f'/model/{name}/pose',
+                                     'pose_gt', 'geometry_msgs/Pose', GazeboBridge.gz2ros))
+            sl.node('pose_to_tf', parameters={'child_frame': name + '/base_footprint'})
+        else:
+            bridges.append(GazeboBridge(f'/model/{name}/odometry',
+                            'odom', 'nav_msgs/Odometry', GazeboBridge.gz2ros))
+            bridges.append(GazeboBridge(f'/model/{name}/tf',
+                        '/tf', 'tf2_msgs/msg/TFMessage', GazeboBridge.gz2ros))
         
         bridges.append(GazeboBridge(GazeboBridge.model_topic(name, 'cmd_vel'),
                                      'cmd_vel', 'geometry_msgs/Twist', GazeboBridge.ros2gz))
 
-        bridges.append(GazeboBridge(name / 'imu', 'imu', 'sensor_msgs/Imu', GazeboBridge.gz2ros))
+        bridges.append(GazeboBridge(name+'/imu', 'imu', 'sensor_msgs/Imu', GazeboBridge.gz2ros))
 
-        bridges.append(GazeboBridge(name / 'image', 'image', 'sensor_msgs/Image', GazeboBridge.gz2ros))
+        bridges.append(GazeboBridge(name+'/image', 'image', 'sensor_msgs/Image', GazeboBridge.gz2ros))
 
-        bridges.append(GazeboBridge(name / 'scan', 'scan', 'sensor_msgs/LaserScan', GazeboBridge.gz2ros))
+        bridges.append(GazeboBridge(name+'/scan', 'scan', 'sensor_msgs/LaserScan', GazeboBridge.gz2ros))
 
         sl.create_gz_bridge(bridges)
         
-        with sl.group(if_arg='gui'):
+        if sl.arg('gui'):
             sl.node('slider_publisher', arguments=[sl.find('slider_publisher', 'Twist.yaml')])
 
     return sl.launch_description()
+
+
+generate_launch_description = sl.launch_description(launch_setup)
